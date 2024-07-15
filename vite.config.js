7c599bf91bc6c9ae5888ca02d92e197350257160
firajest <boma385@gmail.com>
2024-07-15 16:10:59 +0300

    Deploy preparations. Intall gh-pages and change configs

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [
     vue(),
     vueDevTools(),
@@ -14,5 +14,7 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
-})
+  },
+  base: mode === 'production' ? 'tages-vue' : '/',
+  
+}))
